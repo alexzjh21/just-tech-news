0b453e21af67cc18b06718d355abd4f938cc1ea0
alexzjh21 <alexzjh21@yahoo.co.jp>
2022-03-23 00:42:24 -0400

    handle login/logout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
+    console.log(req.session);
     Post.findAll({
       attributes: [
         'id',
@@ -34,6 +35,10 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
 });
 
 module.exports = router;
